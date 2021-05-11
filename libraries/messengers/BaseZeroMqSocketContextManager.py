0656b66d141d4e2517180c52e85149403acf2982
Gabriel Espanhol Santos <gabrielmail09@gmail.com>
2021-05-11 04:04:12 -0300

    Fixing the client and server implementation, Adding a remove_socket_from_context method to the BaseZeroMManager

--- libraries/messengers/BaseZeroMqSocketContextManager.py
+++ libraries/messengers/BaseZeroMqSocketContextManager.py
@@ -1,5 +1,5 @@
 import zmq
-from zmq import REP, NOBLOCK
+from zmq import REP
 from collections import namedtuple
 
 from ..constants.constants import TYPE_ERROR_MESAGE, VALUE_ERROR_MESSAGE
@@ -8,10 +8,12 @@ ADDRESS_PATTERN = '{protocol}://{interface}:{port}'
 DEFAULT_MESSAGE_SEPARATOR = ':'.encode()
 SocketIteratorResult = namedtuple('SocketIteratorResult', ['name', 'content'])
 
+SOCKET_NAME_RESTRICTION = "'socket_name' must be equal to some of these values {}"
+
 
 class BaseZeroMqSocketContextManager:
 
-    def __init__(self, init_context: bool=True, **kwargs):
+    def __init__(self, init_context: bool = True, **kwargs):
         self.create_sockets_context(kwargs.get('io_threads', 1))
 
         self.__sockets = {}
@@ -21,31 +23,48 @@ class BaseZeroMqSocketContextManager:
     def sockets_context(self):
         return self.__sockets_context
 
-    def create_sockets_context(self, io_threads: int=1):
+    def create_sockets_context(self, io_threads: int = 1):
         self.__sockets_context = zmq.Context(io_threads)
-    
+
     @property
     def sockets(self):
         return self.__sockets
-    
-    def add_socket_to_context(self, socket_name: str, socket_type: int=REP):
+
+    def add_socket_to_context(self, socket_name: str, socket_type: int = REP):
         if isinstance(socket_name, str):
             self.sockets[socket_name] = {
                     'socket': self.sockets_context.socket(socket_type)
                 }
             self.__socket_names.append(socket_name)
         else:
-            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name', tp=str,
+            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name',
+                                                     tp=str,
                                                      inst=type(socket_name)))
-    
-    def connect_socket_to_address(self, 
-                                  socket_name: str,
-                                  address_protocol: str='tcp',
-                                  address_interface: str='127.0.0.1',
-                                  address_port: str='5555'):
+
+    def remove_socket_from_context(self, socket_name: str):
         if isinstance(socket_name, str):
             if socket_name in self.__socket_names:
-                
+                self.sockets.pop(socket_name)
+                self.__socket_names.remove(socket_name)
+            else:
+                raise ValueError(VALUE_ERROR_MESSAGE.format(
+                    param='socket_name',
+                    restr=(SOCKET_NAME_RESTRICTION
+                           .format(self.__socket_names)))
+                )
+        else:
+            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name',
+                                                     tp=str,
+                                                     inst=type(socket_name)))
+
+    def connect_socket_to_address(self,
+                                  socket_name: str,
+                                  address_protocol: str = 'tcp',
+                                  address_interface: str = '127.0.0.1',
+                                  address_port: int = 5555):
+        if isinstance(socket_name, str):
+            if socket_name in self.__socket_names:
+
                 self.sockets[socket_name]['socket'].connect(
                     ADDRESS_PATTERN.format(protocol=address_protocol,
                                            interface=address_interface,
@@ -60,13 +79,14 @@ class BaseZeroMqSocketContextManager:
             else:
                 raise ValueError(VALUE_ERROR_MESSAGE.format(
                     param='socket_name',
-                    restr=("'socket_name' must be equal to some of these values {}"
+                    restr=(SOCKET_NAME_RESTRICTION
                            .format(self.__socket_names)))
                 )
         else:
-            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name', tp=str,
+            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name',
+                                                     tp=str,
                                                      inst=type(socket_name)))
-    
+
     def disconnect_socket_from_address(self, socket_name: str):
         if isinstance(socket_name, str):
             if socket_name in self.__socket_names:
@@ -76,21 +96,22 @@ class BaseZeroMqSocketContextManager:
             else:
                 raise ValueError(VALUE_ERROR_MESSAGE.format(
                     param='socket_name',
-                    restr=("'socket_name' must be equal to some of these values {}"
+                    restr=(SOCKET_NAME_RESTRICTION
                            .format(self.__socket_names)))
                 )
         else:
-            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name', tp=str,
+            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name',
+                                                     tp=str,
                                                      inst=type(socket_name)))
 
-    def bind_socket_to_address(self, 
+    def bind_socket_to_address(self,
                                socket_name: str,
-                               address_protocol: str='tcp',
-                               address_interface: str='127.0.0.1',
-                               address_port: str='5555'):
+                               address_protocol: str = 'tcp',
+                               address_interface: str = '127.0.0.1',
+                               address_port: int = 5555):
         if isinstance(socket_name, str):
             if socket_name in self.__socket_names:
-                
+
                 self.sockets[socket_name]['socket'].bind(
                     ADDRESS_PATTERN.format(protocol=address_protocol,
                                            interface=address_interface,
@@ -107,11 +128,12 @@ class BaseZeroMqSocketContextManager:
             else:
                 raise ValueError(VALUE_ERROR_MESSAGE.format(
                     param='socket_name',
-                    restr=("'socket_name' must be equal to some of these values {}"
+                    restr=(SOCKET_NAME_RESTRICTION
                            .format(self.__socket_names)))
                 )
         else:
-            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name', tp=str,
+            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name',
+                                                     tp=str,
                                                      inst=type(socket_name)))
 
     def unbind_socket_from_address(self, socket_name: str):
@@ -123,13 +145,14 @@ class BaseZeroMqSocketContextManager:
             else:
                 raise ValueError(VALUE_ERROR_MESSAGE.format(
                     param='socket_name',
-                    restr=("'socket_name' must be equal to some of these values {}"
+                    restr=(SOCKET_NAME_RESTRICTION
                            .format(self.__socket_names)))
                 )
         else:
-            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name', tp=str,
+            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name',
+                                                     tp=str,
                                                      inst=type(socket_name)))
-    
+
     def receive_message(self, socket_name: str) -> list:
         if isinstance(socket_name, str):
             if socket_name in self.__socket_names:
@@ -140,44 +163,49 @@ class BaseZeroMqSocketContextManager:
             else:
                 raise ValueError(VALUE_ERROR_MESSAGE.format(
                     param='socket_name',
-                    restr=("'socket_name' must be equal to some of these values {}"
+                    restr=(SOCKET_NAME_RESTRICTION
                            .format(self.__socket_names)))
                 )
         else:
-            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name', tp=str,
+            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name',
+                                                     tp=str,
                                                      inst=type(socket_name)))
-    
+
     def send_message(self,
                      socket_name: str,
                      data: bytes,
-                     **kwargs)  -> object:
+                     **kwargs) -> object:
         if isinstance(socket_name, str):
             if socket_name in self.__socket_names:
                 result = self.sockets[socket_name]['socket'].send(
-                    data=(kwargs.get('received_from', socket_name).encode() + 
-                          DEFAULT_MESSAGE_SEPARATOR + 
-                          data)
+                    data=(kwargs.get('message_sender', socket_name).encode() +
+                          DEFAULT_MESSAGE_SEPARATOR +
+                          data +
+                          DEFAULT_MESSAGE_SEPARATOR +
+                          (str(kwargs.get('message_recipient', socket_name))
+                           .encode()))
                 )
 
                 return result
             else:
                 raise ValueError(VALUE_ERROR_MESSAGE.format(
                     param='socket_name',
-                    restr=("'socket_name' must be equal to some of these values {}"
+                    restr=(SOCKET_NAME_RESTRICTION
                            .format(self.__socket_names)))
                 )
         else:
-            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name', tp=str,
+            raise TypeError(TYPE_ERROR_MESAGE.format(param='socket_name',
+                                                     tp=str,
                                                      inst=type(socket_name)))
-    
+
     def __iter__(self):
         self.__iter_counter = 0
         return self
-    
+
     def __next__(self):
         if self.__iter_counter == len(self.__socket_names):
             raise StopIteration
-        
+
         result = self.sockets[self.__socket_names[self.__iter_counter]]
         self.__iter_counter += 1
 
@@ -185,4 +213,3 @@ class BaseZeroMqSocketContextManager:
             name=self.__socket_names[self.__iter_counter],
             content=result
         )
-
